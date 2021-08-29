80e8eb0588f6e54a38ba61b85621361b9221cb37
takahiro-ngc <takahiro0116@gmail.com>
2021-08-29 21:07:47 +0900

    pusyuテスト

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   //   // !! 警告 !!
   //   // あなたのプロジェクトに型エラーがあったとしても、プロダクションビルドを正常に完了するために危険な許可をする。
   //   // !! 警告 !!
-  //   ignoreBuildErrors: true,
+  ignoreBuildErrors: true,
   // },
 };
