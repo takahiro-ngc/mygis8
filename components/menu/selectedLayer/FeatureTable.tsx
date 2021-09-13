541bc4ee465d8756c39eb2771ac5e0366cc3ba70
takahiro-ngc <takahiro0116@gmail.com>
2021-09-13 20:44:48 +0900

    test

--- components/menu/selectedLayer/FeatureTable.tsx
+++ components/menu/selectedLayer/FeatureTable.tsx
@@ -1,60 +1,60 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import { DataGrid, GridColDef, GridSelectionModel } from "@mui/x-data-grid";
-import renderCellExpand from "./renderCellExpand";
-import { getCenterPosition } from "../../utility";
-import { jumpSetting } from "../../utility";
-// import { CustomToolbar, CustomPagination } from "./DataGridCompornent";
+// import { DataGrid, GridColDef, GridSelectionModel } from "@mui/x-data-grid";
+// import renderCellExpand from "./renderCellExpand";
+// import { getCenterPosition } from "../../utility";
+// import { jumpSetting } from "../../utility";
+// // import { CustomToolbar, CustomPagination } from "./DataGridCompornent";
 
-const FeatureTable = ({ features, setViewState }) => {
-  const keyList = features?.flatMap((d) => Object.keys(d?.properties));
-  const uniqueKeyList = Array.from(new Set(keyList));
-  const columns: GridColDef[] = uniqueKeyList.map((d) => ({
-    field: d,
-    headerName: d,
-    minWidth: 150,
-    renderCell: renderCellExpand,
-  }));
+// const FeatureTable = ({ features, setViewState }) => {
+//   const keyList = features?.flatMap((d) => Object.keys(d?.properties));
+//   const uniqueKeyList = Array.from(new Set(keyList));
+//   const columns: GridColDef[] = uniqueKeyList.map((d) => ({
+//     field: d,
+//     headerName: d,
+//     minWidth: 150,
+//     renderCell: renderCellExpand,
+//   }));
 
-  const rows = features?.map((d, index) => ({ id: index, ...d?.properties }));
+//   const rows = features?.map((d, index) => ({ id: index, ...d?.properties }));
 
-  const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
-  const selectedFeature = features[selectionModel[0]];
-  const position = getCenterPosition(selectedFeature);
-  const jump = () =>
-    position.length &&
-    setViewState((prev) => ({
-      ...prev,
-      longitude: position[0],
-      latitude: position[1],
-      ...jumpSetting,
-    }));
+//   const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
+//   const selectedFeature = features[selectionModel[0]];
+//   const position = getCenterPosition(selectedFeature);
+//   const jump = () =>
+//     position.length &&
+//     setViewState((prev) => ({
+//       ...prev,
+//       longitude: position[0],
+//       latitude: position[1],
+//       ...jumpSetting,
+//     }));
 
-  // const Footer = () => <CustomPagination jump={jump} />;
-  return (
-    <>
-      {columns.length ? (
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          disableColumnMenu
-          // components={{
-          //   Toolbar: CustomToolbar,
-          //   Footer: Footer,
-          // }}
-          onSelectionModelChange={(newSelectionModel) => {
-            setSelectionModel(newSelectionModel);
-          }}
-          selectionModel={selectionModel}
-          style={{ height: "700px", maxHeight: "40vh" }}
-          headerHeight={32}
-          rowHeight={36}
-        />
-      ) : (
-        "このデータは，情報が登録されていないようです。"
-      )}
-    </>
-  );
-};
+//   // const Footer = () => <CustomPagination jump={jump} />;
+//   return (
+//     <>
+//       {columns.length ? (
+//         <DataGrid
+//           rows={rows}
+//           columns={columns}
+//           disableColumnMenu
+//           // components={{
+//           //   Toolbar: CustomToolbar,
+//           //   Footer: Footer,
+//           // }}
+//           onSelectionModelChange={(newSelectionModel) => {
+//             setSelectionModel(newSelectionModel);
+//           }}
+//           selectionModel={selectionModel}
+//           style={{ height: "700px", maxHeight: "40vh" }}
+//           headerHeight={32}
+//           rowHeight={36}
+//         />
+//       ) : (
+//         "このデータは，情報が登録されていないようです。"
+//       )}
+//     </>
+//   );
+// };
 
-export default React.memo(FeatureTable);
+// export default React.memo(FeatureTable);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,31 +30,30 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
+
+  // `getInitialProps` belongs to `_document` (instead of `_app`),
+  // it's compatible with server-side generation (SSG).
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
+  }
 }
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with server-side generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
-  // static async getInitialProps(
-  //   ctx: DocumentContext
-  // ): Promise<DocumentInitialProps> {
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
-// }
